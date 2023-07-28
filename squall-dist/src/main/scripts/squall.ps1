b079f72a5b419656185148f2d0a61c41c631ca7a
leadpony <leadpony@openclosed.dev>
2023-07-29 00:08:40 +0900

    Support JAVA_17_HOME environment variable

--- squall-dist/src/main/scripts/squall.ps1
+++ squall-dist/src/main/scripts/squall.ps1
@@ -3,8 +3,10 @@ $scriptDir = Split-Path $scriptPath -Parent
 $homeDir = Split-Path $scriptDir -Parent
 
 $javaCmd = 'java'
-if ($null -ne $env:JAVA_HOME) {
-    $javaCmd = "$env:JAVA_HOME\bin\java"
+if ($null -ne $env:JAVA_17_HOME) {
+  $javaCmd = "$env:JAVA_17_HOME\bin\java"
+} elseif ($null -ne $env:JAVA_HOME) {
+  $javaCmd = "$env:JAVA_HOME\bin\java"
 }
 
 $javaArgs = @('--module-path', "$homeDir\lib", '--module', 'dev.openclosed.squall.cli') + $args
