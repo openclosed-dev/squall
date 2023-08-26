e26533607ab4adc62baf53337fd491e72ef34c1a
leadpony <leadpony@openclosed.dev>
2023-08-26 22:17:11 +0900

    Add --win-upgrade-uuid option for windows msi

--- squall-installer/build.ps1
+++ squall-installer/build.ps1
@@ -9,4 +9,5 @@ Copy-Item -Path 'src/main/legal' -Destination 'target/squall' -Recurse
 
 jpackage --type msi --dest target `
   --app-image target/squall --resource-dir src/main/jpackage/config/windows @target/metadata.txt `
-  --win-per-user-install --temp target/temp --verbose
+  --win-per-user-install --win-upgrade-uuid '421c99bd-78dc-425a-b471-f5d077bb15c1' `
+  --temp target/temp --verbose
