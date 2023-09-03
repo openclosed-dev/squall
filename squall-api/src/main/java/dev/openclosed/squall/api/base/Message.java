fd628de94861197b8d1e9c323c8150becb26e3e3
leadpony <leadpony@openclosed.dev>
2023-09-04 01:46:35 +0900

    Rename Message#getBundle to bundle

--- squall-api/src/main/java/dev/openclosed/squall/api/base/Message.java
+++ squall-api/src/main/java/dev/openclosed/squall/api/base/Message.java
@@ -17,31 +17,21 @@
 package dev.openclosed.squall.api.base;
 
 import java.text.MessageFormat;
+import java.util.Objects;
 import java.util.ResourceBundle;
 import java.util.function.Supplier;
 
 /**
- * A message.
+ * A message in resource bundle.
  */
 public interface Message extends Supplier<String> {
 
     static Message of(String key, Object[] args, ResourceBundle bundle) {
-        return new Message() {
-            @Override
-            public String key() {
-                return key;
-            }
-
-            @Override
-            public Object[] args() {
-                return args;
-            }
-
-            @Override
-            public ResourceBundle getBundle() {
-                return bundle;
-            }
-        };
+        Objects.requireNonNull(key);
+        Objects.requireNonNull(args);
+        Objects.requireNonNull(bundle);
+        record BundledMessage(String key, Object[] args, ResourceBundle bundle) implements Message { }
+        return new BundledMessage(key, args, bundle);
     }
 
     /**
@@ -57,22 +47,24 @@ public interface Message extends Supplier<String> {
     Object[] args();
 
     /**
+     * Returns the resource bundle for this message.
+     * @return the resource bundle found.
+     */
+    ResourceBundle bundle();
+
+    // Supplier
+
+    /**
      * Returns the formatted message.
      * @return the formatted message.
      */
     @Override
     default String get() {
-        var pattern = getBundle().getString(key());
+        var pattern = bundle().getString(key());
         if (args().length > 0) {
             return MessageFormat.format(pattern, args());
         } else {
             return pattern;
         }
     }
-
-    /**
-     * Returns the resource bundle for this message.
-     * @return the resource bundle found.
-     */
-    ResourceBundle getBundle();
 }
