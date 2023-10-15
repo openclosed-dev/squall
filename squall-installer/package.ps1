dd67bed60c49cc8a3d2139d9b0a319490f0fb8a5
leadpony <leadpony@openclosed.dev>
2023-10-15 23:09:10 +0900

    Add zip-format release

--- squall-installer/package.ps1
+++ squall-installer/package.ps1
@@ -1,29 +1,41 @@
+param(
+  [string]$version
+)
+
+if (!$version) {
+  $version = Get-Content -Path ./target/version.txt -ReadCount 1
+}
+
+Write-Host 'Deleting previous output...'
 Remove-Item ./target/runtime -Force -Recurse -ErrorAction SilentlyContinue
 Remove-Item ./target/squall -Force -Recurse -ErrorAction SilentlyContinue
 Remove-Item ./target/temp -Force -Recurse -ErrorAction SilentlyContinue
 Remove-Item ./target/*.msi
+Remove-Item ./target/*.zip
 
 if ($null -ne $env:JAVA_17_HOME) {
   $toolDir = "$env:JAVA_17_HOME\bin"
 } elseif ($null -ne $env:JAVA_HOME) {
   $toolDir = "$env:JAVA_HOME\bin"
 } else {
-  Write-Host 'Please define enviroment variable "JAVA_HOME"'
+  Write-Host 'Please define environment variable "JAVA_HOME"'
   exit 1
 }
 
-# Generates slim Java runtime
+Write-Host 'Generating slim Java runtime...'
 & "$toolDir\jlink" `
   --add-modules java.base,java.logging,java.management,jdk.unsupported `
   --strip-native-commands --strip-debug --no-man-pages --no-header-files `
   --output target/runtime `
   --verbose
 
+Write-Host 'Generating application image...'
 & "$toolDir\jpackage" `
   --type app-image `
   --dest target `
   --runtime-image target/runtime `
-  @target/metadata.txt `
+  --app-version $version `
+  @src/main/jpackage/metadata.txt `
   --input target/input `
   --module-path target/mods `
   --module dev.openclosed.squall.cli `
@@ -33,13 +45,18 @@ if ($null -ne $env:JAVA_17_HOME) {
   --win-console `
   --verbose
 
+Write-Host 'Generating MSI installer...'
 & "$toolDir\jpackage" `
   --type msi `
   --dest target `
   --app-image target/squall `
   --resource-dir src/main/jpackage/config/windows `
-  @target/metadata.txt `
+  --app-version $version `
+  @src/main/jpackage/metadata.txt `
   --win-per-user-install `
   --win-upgrade-uuid '421c99bd-78dc-425a-b471-f5d077bb15c1' `
   --temp target/temp `
   --verbose
+
+Write-Host 'Generating zip archive...'
+Compress-Archive -Path ./target/squall -DestinationPath "./target/squall-${version}-win-x64.zip"
